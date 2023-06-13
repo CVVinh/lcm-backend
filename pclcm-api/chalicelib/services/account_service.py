09ab28980f186909fc40682f98ffe8afeddbe3bd
CVVinh <caovanvinh111@gmail.com>
2023-06-13 17:18:30 +0700

    commit update

--- pclcm-api/chalicelib/services/account_service.py
+++ pclcm-api/chalicelib/services/account_service.py
@@ -1,7 +1,7 @@
-from chalicelib.models.models import AccountMaster, AccountGroupMaster, GroupMaster
+from chalicelib.models.models import AccountMaster, AccountGroupMaster, GroupMaster, AccountBaseMaster, BaseMaster
 from chalicelib.models import session
 from chalicelib.messages import MessageResponse
-
+from chalicelib.utils.utils import object_as_dict, add_update_object, paginate, format_day_and_bool_dict, export
 message_account_constant = MessageResponse()
 message_account_constant.setName("Account Master")
 
@@ -16,12 +16,17 @@ def get_account_list(query_params):
         The message and a list.
     """
     # Query Column needs to get, join tables containing information to get
-    query_list_account = session.query(AccountMaster.accountId, AccountMaster.accountName,
-                                       GroupMaster.groupId, GroupMaster.groupName).join(
+    # query_list_account = session.query(AccountMaster.accountId, AccountMaster.accountName,
+    #                                    GroupMaster.groupId, GroupMaster.groupName, AccountMaster.extAccountId, AccountMaster.emailAddress, AccountMaster.accountName).join(
+    #     AccountGroupMaster, AccountGroupMaster.accountId == AccountMaster.accountId, isouter=True).join(
+    #     GroupMaster, AccountGroupMaster.groupId == GroupMaster.groupId, isouter=True).filter(
+    #     AccountMaster.isDeleted == 0)
+
+    query_list_account = session.query(AccountMaster.accountId).join(
         AccountGroupMaster, AccountGroupMaster.accountId == AccountMaster.accountId, isouter=True).join(
         GroupMaster, AccountGroupMaster.groupId == GroupMaster.groupId, isouter=True).filter(
         AccountMaster.isDeleted == 0)
-
+        
     # Create loop of lists account, assign it to an object and then assign to a new list.
     result_list = [{**account} for account in query_list_account.all()]
     return (True, {"mstAccount": result_list,
